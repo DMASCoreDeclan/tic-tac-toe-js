0c55db454b7f0cdf88d3cffd1d5b437a39375447
DMASCoreDeclan <54984695+DMASCoreDeclan@users.noreply.github.com>
2023-08-20 21:04:24 +0000

    Add constants to javascript.  Add eventlisteners to constants and begin javascript code for handling the clicking og cells in the gameboard area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,56 @@
-const crossClass = 'cross';
-const circleClass = 'circle';
-const cells = document.querySelectorAll('[data-cell]')
+//Define constants for every Game
+const crossClass = 'X';
+const circleClass = 'O';
+const cells = document.querySelectorAll('.cell')
+const player_x = "X";
+const player_o = "O";
+
+// Randomly pic the start player
+// let startturn = ((Math.floor(Math.random() * 2) + 1) > 1) ? player_x : player_o;
+let turn = player_x;
+
+// Create the Board Array to keep track of where players place their O's ans O's
+const gameBoard = Array(cells.length);
+gameBoard.fill("");
+
+//Get element and sound placeholders
+const strike = document.getElementById('strike');
+const gameMessages = document.getElementById('game-messages');
+const gameResult = document.getElementById('game-result');
+const restartButton = document.getElementById('restart-button');
+const clickSound = new Audio('assets/sounds/clickSound.wav')
+const gameOverSound = new Audio('assets/sounds/gameOverSound.wav')
+
+cells.forEach((cell) => cell.addEventListener('click', handleClick));
+
+
+function handleClick(clickedCell) {
+    //If the game is over, do not execute
+    if (gameMessages.classList.contains('show')) {
+        return;
+    };
+    
+    const cell = clickedCell.target;
+    const cellNumber = parseInt(cell.dataset.cellIndex) - 1;
+    
+    //If the cell is not empty, do not execute
+    if (cell.innerText != "") {
+        return;
+    }; 
+
+    if (turn === player_x) {
+        cell.innerText = player_x
+        gameBoard[cellNumber] = player_x;
+        turn = player_o;
+    } else {
+        cell.innerText = player_o
+        gameBoard[cellNumber] = player_o;
+        turn = player_x;
+    }
+
+    clickSound.volume = .05; 
+    clickSound.play();
+
+    console.log(turn,  cell, cellNumber, gameBoard)
+};
+
