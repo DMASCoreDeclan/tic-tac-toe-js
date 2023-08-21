9b3d5984825463945af6ee0225ff7f59c8e28288
DMASCoreDeclan <54984695+DMASCoreDeclan@users.noreply.github.com>
2023-08-21 09:38:19 +0000

    Add checkresult logic in javascript to apply css strikethrough

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,7 @@ const restartButton = document.getElementById('restart-button');
 const clickSound = new Audio('assets/sounds/clickSound.wav')
 const gameOverSound = new Audio('assets/sounds/gameOverSound.wav')
 
+//Add an Event Listener to each cell and determine what to do when a cell is clicked
 cells.forEach((cell) => cell.addEventListener('click', handleClick));
 
 //Apply a Hover Effect to indicate whose turn it is
@@ -49,6 +50,7 @@ function handleClick(clickedCell) {
         return;
     };
     
+    //Store the cell number that the user clicked on and get the cell number of that tile 
     const cell = clickedCell.target;
     const cellNumber = parseInt(cell.dataset.cellIndex) - 1;
     
@@ -56,7 +58,7 @@ function handleClick(clickedCell) {
     if (cell.innerText != "") {
         return;
     }; 
-    // Place a mark of an X or an O on the gameBoard
+    // If the cell is empty, place a mark of an X or an O on the gameBoard
     if (turn === player_x) {
         cell.innerText = player_x
         gameBoard[cellNumber] = player_x;
@@ -66,11 +68,57 @@ function handleClick(clickedCell) {
         gameBoard[cellNumber] = player_o;
         turn = player_x;
     }
-
+    //Play sound when a cell is clicked
     clickSound.volume = .05; 
     clickSound.play();
 
+    //Indicate an X or an O for where the current player can choose his next cell
     hoverText();
-    console.log(turn,  cells, cellNumber, gameBoard)
+    //Check to see if the last cell occupied produces a Winner or a Draw
+    checkResult();
+
+
+    // console.log(turn,  cells, cellNumber, gameBoard, checkResult)
 };
+
+const winningPossibilities = [
+    //Row
+    {combination: [0, 1, 2], strikeClass: "strike-row-1"},
+    {combination: [3, 4, 5], strikeClass: "strike-row-2"},
+    {combination: [6, 7, 8], strikeClass: "strike-row-3"},
+    //Columns
+    {combination: [0, 3, 6], strikeClass: "strike-col-1"},
+    {combination: [1, 4, 7], strikeClass: "strike-col-2"},
+    {combination: [2, 5, 8], strikeClass: "strike-col-3"},
+    //Diagonals
+    { combination: [2, 4, 6], strikeClass: "strike-diagonal-1"},
+    {combination: [0, 4, 8], strikeClass: "strike-diagonal-2"}
+    
+]
+
+
+function checkResult() {
+    //Check for Winner
+    for(const winningPosibility of winningPossibilities) {
+        //If the clicked cells have the same number as the winningPossibility, identify the corresponding strikeClass
+        // const combination = winningPossibilities.combination;
+        // const strikeClass = winningPossibilities.strikeClass;
+        //Object Destructuring, instead of using the two lines of code above
+        const { combination, strikeClass } = winningPosibility;
+        const cellContents1 = gameBoard[combination[0]]; 
+        const cellContents2 = gameBoard[combination[1]]; 
+        const cellContents3 = gameBoard[combination[2]]; 
+
+        if (cellContents1 != "" && cellContents1 === cellContents2 && cellContents1 === cellContents3) {
+            strike.classList.add(strikeClass)
+            console.log(combination, strikeClass)
+        };
+        
+        // console.log(cellContents1, cellContents2, cellContents3);
+    };
+    //Check for Draw
+    
+    
+};
+
 
